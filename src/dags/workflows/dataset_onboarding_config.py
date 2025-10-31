a930eb758ee5e9eb6c09f410bcd737a0bab4d524
Voskakis <constantinosvoskakis@hotmail.gr>
2025-10-31 15:14:28 +0200

    #1 Refactor dataset onboarding config parameters
    Replaces default values in DAG_PARAMS with empty lists, adds new parameters (code, version, mime_type, date_published), and updates the config_onboarding payload to use these parameters. Removes hardcoded and example values, ensuring all fields are sourced from DAG context.

--- src/dags/workflows/dataset_onboarding_config.py
+++ src/dags/workflows/dataset_onboarding_config.py
@@ -1,4 +1,4 @@
-import json
+import datetime
 import os
 import uuid
 
@@ -16,11 +16,12 @@ DAG_ID = "DATASET_ONBOARDING"
 
 DAG_PARAMS = {
     "id": Param(type="string", format="uuid"),
+    "code": Param(type="string"),
     "name": Param(type="string"),
     "description": Param(type="string"),
     "headline": Param(type="string"),
     "fields_of_science": Param([], type="array"),
-    "languages": Param(["en", "es"], type="array"),
+    "languages": Param([], type="array"),
     "keywords": Param([], type="array"),
     "countries": Param([], type="array"),
     "publishedUrl": Param(type="string", format="uri"),
@@ -28,45 +29,36 @@ DAG_PARAMS = {
     "citeAs": Param("", type="string"),
     "license": Param(type="string"),
     "size": Param(0, type="integer", minimum=0),
-    "dataLocations": Param(json.dumps(
-        [
-            {
-                "kind": "File",  # other options: http, ftp, remote
-                "url": "/opt/airflow/config/folder/train-images.idx3-ubyte"
-            },
-            {
-                "kind": "Ftp",  # other options: http, ftp, remote
-                "url": "ftp://my-ftp-server:21/train-images.idx3-ubyte"
-            },
-            {
-                "kind": "Http",  # other options: http, ftp, remote
-                "url": "https://tse3.mm.bing.net/th/id/OIP.U_VJuupQohwnzXcKMztqWgHaEo?rs=1&pid=ImgDetMain&o=7&rm=3"
-            }
-        ]), type="string"),
+    "dataLocations": Param([], type="string"),
+    "version": Param(type="string"),
+    "mime_type": Param(type="string"),
+    "date_published": Param(f"{datetime.date.today()}", type="string", format="date"),
+
 }
 
 DAG_TAGS = ["DatasetOnboarding", ]
 
 
-def config_onboarding(auth_token: str, dag_context: Context, config: GatewayConfig, data_locations: list[tuple[str, str]]):
+def config_onboarding(auth_token: str, dag_context: Context, config: GatewayConfig,
+                      data_locations: list[tuple[str, str]]):
     gateway_url: str = config.options.base_url + config.options.dataset.onboarding_mock
     payload = {
         "Id": dag_context["params"]["id"],
         "Code": dag_context["params"]["id"],
         "Name": dag_context["params"]["name"],
         "Description": dag_context["params"]["description"],
-        "License": dag_context["params"]["license"],
-        "MimeType": "my-mime",
-        "Size": dag_context["params"]["size"],
-        "Url": dag_context["params"]["publishedUrl"],
-        "Version": "v1",
         "Headline": dag_context["params"]["headline"],
-        "Keywords": dag_context["params"]["keywords"],
         "FieldOfScience": dag_context["params"]["fields_of_science"],
         "Language": dag_context["params"]["languages"],
+        "Keywords": dag_context["params"]["keywords"],
         "Country": dag_context["params"]["countries"],
-        "DatePublished": "2025-10-23",
-        "DataLocations": [{"kind": k, "url": u} for k, u in data_locations]
+        "Url": dag_context["params"]["publishedUrl"],
+        "License": dag_context["params"]["license"],
+        "Size": dag_context["params"]["size"],
+        "DataLocations": [{"kind": k, "url": u} for k, u in data_locations],
+        "Version": dag_context["params"]["version"],
+        "MimeType": dag_context["params"]["license"],
+        "DatePublished": dag_context["params"]["date_published"],
     }
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {auth_token}",
                "Connection": "keep-alive"}
