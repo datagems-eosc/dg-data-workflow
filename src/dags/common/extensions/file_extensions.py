4b6121db06b96f0bdb45b427bac98343640cb3d8
voskakiscite <kvoskakis@cite.gr>
2026-02-10 13:25:02 +0200

    unique names with _ as per profiler tastes

--- src/dags/common/extensions/file_extensions.py
+++ src/dags/common/extensions/file_extensions.py
@@ -28,7 +28,7 @@ def process_location(guid: str, location: DataLocation, stream_service: DataRetr
         with stream_service.retrieve(location) as retrieved_file:
             base_name = Path(retrieved_file.file_name).stem
             extension = retrieved_file.file_extension
-            unique_name = f"{base_name}.{uuid.uuid4()}"
+            unique_name = f"{base_name}_{uuid.uuid4()}"
             full_path = os.fspath(build_file_path(config.local_staging_path, guid, unique_name, extension))
             should_decode = location.kind is DataLocationKind.Http
             stage_service.store(retrieved_file.stream, full_path, decode=should_decode)
