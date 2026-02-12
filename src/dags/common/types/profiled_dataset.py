3be13bc3a2ac6818c6190d17227f8c79e2d24e94
voskakiscite <kvoskakis@cite.gr>
2026-02-12 10:26:20 +0200

    remove unused mappers

--- src/dags/common/types/profiled_dataset.py
+++ src/dags/common/types/profiled_dataset.py
@@ -13,75 +13,6 @@ class LDModel(BaseModel):
 
 
 @dataclass
-class FieldSourceExtract(LDModel):
-    model_config = {
-        "populate_by_name": True,
-        "extra": "allow"
-    }
-    column: Optional[str] = None
-
-
-@dataclass
-class FieldSource(LDModel):
-    model_config = {
-        "populate_by_name": True,
-        "extra": "allow"
-    }
-    type: str = PyField(None, alias="@type")
-    id: str = PyField(None, alias="@id")
-    fileObject: Optional[Dict[str, str]] = None
-    extract: Optional[FieldSourceExtract] = None
-
-
-@dataclass
-class StatisticsHistogram(LDModel):
-    model_config = {
-        "populate_by_name": True,
-        "extra": "allow"
-    }
-    count: Optional[int] = None
-    binRange: Optional[List[float]] = None
-
-
-@dataclass
-class FieldStatistics(LDModel):
-    model_config = {
-        "populate_by_name": True,
-        "extra": "allow"
-    }
-    type: str = PyField(None, alias="@type")
-    id: str = PyField(None, alias="@id")
-    histogram: Optional[List[StatisticsHistogram]] = PyField(None, alias="dg:histogram")
-    fileObject: Optional[Dict[str, str]] = None
-    extract: Optional[FieldSourceExtract] = None
-
-
-@dataclass
-class Field(LDModel):
-    model_config = {
-        "populate_by_name": True,
-        "extra": "allow"
-    }
-    type: str = PyField(None, alias="@type")
-    id: str = PyField(None, alias="@id")
-    dataType: Optional[str] = None
-    source: Optional[FieldSource] = None
-    statistics: Optional[FieldStatistics] = None
-    sample: Optional[List[Any]] = None
-
-
-@dataclass
-class RecordSet(LDModel):
-    model_config = {
-        "populate_by_name": True,
-        "extra": "allow"
-    }
-    type: str = PyField(None, alias="@type")
-    id: str = PyField(None, alias="@id")
-    field: Optional[List[Field]] = None
-
-
-@dataclass
 class DistributionItem(LDModel):
     model_config = {
         "populate_by_name": True,
@@ -101,4 +32,4 @@ class DatasetResponse(LDModel):
     type: str = PyField(None, alias="@type")
     id: str = PyField(None, alias="@id")
     distribution: Optional[List[DistributionItem]] = None
-    recordSet: Optional[List[RecordSet]] = None
+    recordSet: Optional[List[Any]] = None
