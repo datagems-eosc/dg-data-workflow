0c85156a55727a89971ea5c952b0c3ebb85bfe81
voskakiscite <kvoskakis@cite.gr>
2026-04-21 10:35:55 +0300

    correct use of env variable

--- src/dags/services/dataset_packaging/implementations.py
+++ src/dags/services/dataset_packaging/implementations.py
@@ -5,6 +5,6 @@ from configurations import DatasetPackagingConfig
 
 def dataset_packaging_builder(auth_token: str, dag_context: Context, config: DatasetPackagingConfig) -> tuple[
     str, dict[str, str]]:
-    url: str = config.options.base_url + config.options.profiler.get_profile.format(id=dag_context["params"]["id"])
+    url: str = config.options.base_url + config.options.endpoints.add.format(id=dag_context["params"]["id"])
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {auth_token}", "Connection": "keep-alive"}
     return url, headers
