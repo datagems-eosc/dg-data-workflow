b9db17ede12e5a5fd58ba8fa076a6dd0877ab16d
voskakiscite <kvoskakis@cite.gr>
2026-05-26 12:39:39 +0300

    Update dataset_profiling_full.py

--- src/dags/workflows/dataset_profiling_full.py
+++ src/dags/workflows/dataset_profiling_full.py
@@ -138,6 +138,8 @@ def dataset_profiling():
 
     fetched_heavy_profile = fetch_profile(heavy_fetched_id)
 
+    _ = ingest_cdd(fetched_heavy_profile)
+
     converted_heavy = convert_profiling(fetched_heavy_profile, MomaProfileType.HEAVY.value)
 
     data_management_heavy_id = update_data_management(converted_heavy, fetched_heavy_profile,
