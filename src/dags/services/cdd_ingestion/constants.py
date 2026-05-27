7e93fca0d9905edcdd342d3cf1d7f511ae6d3351
voskakiscite <kvoskakis@cite.gr>
2026-05-27 12:27:40 +0300

    #11 change tag to match casing with other tags

--- src/dags/services/cdd_ingestion/constants.py
+++ src/dags/services/cdd_ingestion/constants.py
@@ -8,7 +8,7 @@ DAG_PARAMS = {
     "id": Param("00000000-0000-0000-0000-000000000000", type=["string"], format="uuid"),
 }
 
-DAG_TAGS = ["CDD_INGEST", ]
+DAG_TAGS = ["CDD_Ingest", ]
 
 MINUTES_TIMEDELTA = 10
 
