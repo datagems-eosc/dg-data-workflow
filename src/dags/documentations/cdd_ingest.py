00d0e50c8d150effa6cdb2c9334002d4ec155d97
voskakiscite <kvoskakis@cite.gr>
2026-05-27 11:35:56 +0300

    #11 update ids with underscores

--- src/dags/documentations/cdd_ingest.py
+++ src/dags/documentations/cdd_ingest.py
@@ -4,20 +4,20 @@ DAG_DESCRIPTION = """
 TODO
 """
 
-CDD_FETCH_FILE_ID = "fetch ingestion file"
+CDD_FETCH_FILE_ID = "fetch_ingestion_file"
 
 CDD_FETCH_FILE_DOC = """
 TODO
 """
 
-CDD_BEGIN_INGEST_ID = "begin ingestion"
+CDD_BEGIN_INGEST_ID = "begin_ingestion"
 
 CDD_BEGIN_INGEST_DOC = """
 TODO
 """
 
 
-WAIT_FOR_COMPLETION_ID = "wait for completion"
+WAIT_FOR_COMPLETION_ID = "wait_for_completion"
 
 WAIT_FOR_COMPLETION_DOC = """
 TODO
