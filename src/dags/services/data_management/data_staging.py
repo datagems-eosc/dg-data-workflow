9657659ed926070effcda863a9196bdc3cbb67db
voskakiscite <kvoskakis@cite.gr>
2026-08-18 12:15:07 +0300

    Update data_staging.py

--- src/dags/services/data_management/data_staging.py
+++ src/dags/services/data_management/data_staging.py
@@ -1,14 +1,18 @@
 import os
+import shutil
 from io import IOBase
 
 
 class DataStagingService:
 
-    def __init__(self):
-        pass
-
-    def store(self, stream: IOBase, destination_path: str, decode: bool = False) -> str:
+    def store(self, stream: IOBase, destination_path: str, decode: bool = False, ) -> str:
         os.makedirs(os.path.dirname(destination_path), exist_ok=True)
-        with open(destination_path, "wb") as f:
-            f.write(stream.read(decode_content=decode))
+
+        with open(destination_path, "wb") as destination:
+            if decode:
+                content = stream.read(decode_content=True)
+                destination.write(content)
+            else:
+                shutil.copyfileobj(stream, destination)
+
         return os.path.abspath(destination_path)
